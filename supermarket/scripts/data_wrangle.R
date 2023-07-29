ccfaf1a55db13ab6af9cf742e644c990194bf0be
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2023-07-29 14:04:47 +0100

    data transformation working with str_replace

--- supermarket/scripts/data_wrangle.R
+++ supermarket/scripts/data_wrangle.R
@@ -9,10 +9,18 @@ library(tidyverse)
 library(readxl)
 
 # load supermarket sales data 
-df_supermarket <- read_csv('data/supermarket_sales - Sheet1.csv')
-
-df_supermarket <- read_xlsx('data/supermarket_sales - Sheet1.csv', sheet = 'supermarket_sales - Sheet1')
+df_supermarket_raw <- read_csv('data/supermarket_sales - Sheet1.csv')
 
 
+# wrangle supermarket sales data ------------------------------------------------------------
 
-head(df_supermarket)
+# data transformation and approximately
+df_supermarket_raw %>%
+  select_all(tolower) %>% 
+  select_all(~str_replace(., " ", "_")) %>% 
+  mutate(invoice_id = invoice_id %>% 
+           str_replace(.,"-","") %>% 
+           str_replace(., "-", "") %>% 
+           as.integer())
+  mutate(invoice_id = str_replace(str_replace(invoice_id, "-",""), "-", ""))
+         
