e216e14bf02bb38ec34614a8ddfe0bdd1184a37a
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2023-07-30 14:31:04 +0100

    contributed to data_wrangling.R script

---
+++ supermarket/README.md
@@ -0,0 +1,9 @@
+## Supermarket Sales Data Dashboard
+
+This is an attempt to develop enterprise dashboard for this supermarket to monitor the income performance.
+
+# To DO
+- Design a dashboard wireframe
+- Data wrangling
+- Understanding of the data
+- 
--- supermarket/scripts/data_wrangle.R
+++ supermarket/scripts/data_wrangle.R
@@ -7,20 +7,37 @@
 
 library(tidyverse)
 library(readxl)
+library(lubridate)
+library(sf)
+library(tmaptools)
 
 # load supermarket sales data 
 df_supermarket_raw <- read_csv('data/supermarket_sales - Sheet1.csv')
 
-
 # wrangle supermarket sales data ------------------------------------------------------------
 
 # data transformation and approximately
-df_supermarket_raw %>%
+df_supermarket <- df_supermarket_raw %>%
   select_all(tolower) %>% 
+  select_all(~str_replace(., " ", "_")) %>% 
   select_all(~str_replace(., " ", "_")) %>% 
   mutate(invoice_id = invoice_id %>% 
            str_replace(.,"-","") %>% 
            str_replace(., "-", "") %>% 
-           as.integer())
-  mutate(invoice_id = str_replace(str_replace(invoice_id, "-",""), "-", ""))
+           as.integer(),
+         branch = as.factor(city), 
+         gender = as.factor(gender),
+         customer_type = as.factor(customer_type),
+         payment = as.factor(payment),
+         date = mdy(date))
+
+df_supermarket %>% head() %>% View()
+
+# To do: 
+# Drop gross_margin_percentage
+# Add a location column to the data
+
+# add a location region
+
+nominatim_loc_geo <- geocode_OSM(df_supermarket$city, details = FALSE, as.data.frame = TRUE)
          
