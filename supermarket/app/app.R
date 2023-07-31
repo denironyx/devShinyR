7553d17e1a995d0b21959c3f4a25b2f548a2fd6e
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2023-07-31 21:35:23 +0100

    added files and worked on the dashboard

--- supermarket/app/app.R
+++ supermarket/app/app.R
@@ -2,40 +2,66 @@
 library(shiny)
 library(bs4Dash)
 library(thematic)
+library(waiter)
 
-thematic_shiny()
+# Load up a font
+system('fc-cache -f ~/.fonts')
+#thematic_shiny()
+purple_1 = "#393B98"
+# Shiny App Layout
 shinyApp(
-  ui = dashboardPage(
-    #preloader = list(html = tagList(spin_2(), "loading ..."), color = "#343a40"),
-    dark = TRUE,
-    help = FALSE,
-    header = dashboardHeader(
+  ui = bs4DashPage(
+    #preloader = list(html = spin_2(), color = "#800080"),
+    preloader = list(html = tagList(spin_1(), "loading ..."), color = "#393B98"),
+    dark = NULL,
+    header = bs4DashNavbar(
+      
+      # navbar
+      skin = "light",
+      border = FALSE,
+      #sidebarIcon = icon("bars"),
+      #compact = TRUE,
+      disable = FALSE,
       title = dashboardBrand(
           title = "Velion, Inc", 
-          color = "primary",
-          href = "",
-          opacity = 0.8,
+          color = "white",
+          href = "https://www.linkedin.com/in/dennis-irorere/",
+          # create a www folder to add your image to it
+          image = "velion_01.jpg",
+          opacity = 0.8
         ),
-        fixed = TRUE),
-    sidebar = dashboardSidebar(
+        fixed = TRUE,
+        rightUi = tagList(
+          dropdownMenu(),
+          userOutput("user")
+          )
+        ),
+    sidebar = bs4DashSidebar(
       fixed = TRUE,
       skin = "light",
-      status = "primary",
-      id = "sidebar",
-      sidebarUserPanel(
-        image = "https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png",
-        name = "Welcome Onboard!"
+      status = "light",
+      bs4SidebarUserPanel(
+        name = " XYZ Customer"
       ),
-      sidebarMenu(
-        id = "sidebar_tab",
-        flat = FALSE,
-        compact = FALSE,
-        sidebarHeader("Overview")
+      
+      # Side bar Menu
+      
+      bs4SidebarMenu(
+        bs4SidebarMenuItem(
+          text = "Overview",
+          tabName = "overview",
+          icon = icon("dashboard")
+        ),
+        bs4SidebarMenuItem(
+          text = "Table",
+          tabName = "table",
+          icon = icon("table")
+        )
       )
     ),
-    body = dashboardBody(),
-    controlbar = dashboardControlbar(),
-    footer = dashboardFooter(
+    body = bs4DashBody(),
+    controlbar = bs4DashControlbar(),
+    footer = bs4DashFooter(
       fixed = FALSE,
       left = a(
         href = "https://twitter.com/denironyx",
@@ -47,6 +73,14 @@ shinyApp(
   ),
   
   server = function(input, output, session){
-    
+    output$user <- renderUser({
+      dashboardUser(
+        name = "Dennis Irorere",
+        image = "dee.png",
+        title = "Senior R developer", 
+        subtitle = "Author",
+        footer = p("The footer", class = "text-center")
+      )
+    })
   }
 )
